df1ff1b2627812a968679d78e77c100ddc9fbde3
egjohnc02 <154654372+egjohnc02@users.noreply.github.com>
2024-06-19 22:05:06 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let box1El = document.getElementById("box1");
+let box2El = document.getElementById("box2");
+
+function randomChar() {
+  let randomNum = Math.floor(Math.random() * characters.length);
+  return characters[randomNum];
+}
+
+function generate() {
+  let randomPass1 = "";
+  let randomPass2 = "";
+  for (i = 0; i < 15; i++) {
+    randomPass1 += randomChar();
+  }
+  box1El.textContent = randomPass1;
+
+  for (i = 0; i < 15; i++) {
+    randomPass2 += randomChar();
+  }
+  box2El.textContent = randomPass2;
+}
